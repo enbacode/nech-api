1796d5d5933a20f6681df2ba460923804d6e5e8d
enba <nick.baas94@gmail.com>
2018-02-19 15:04:58 +0100

    add user nech routes

--- routes/users/index.js
+++ routes/users/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import User from '../../model/user';
+import Nech from '../../model/nech';
 
 let router = express.Router();
 
@@ -15,6 +16,30 @@ router.get('/:name', (req, res) => {
             res.sendStatus(404);
         } else {
             res.json(user);
+        }
+    });
+});
+
+router.get('/:name/nechs', (req, res) => {
+    User.findOne({ username: req.params.name }).then(user => {
+        if (!user) {
+            res.sendStatus(404);
+        } else {
+            Nech.find({ by: user._id }).then(nechs => {
+                res.json(nechs);
+            });
+        }
+    });
+});
+
+router.get('/:name/nechs/:type(nech|klar|trivial)', (req, res) => {
+    User.findOne({ username: req.params.name }).then(user => {
+        if (!user) {
+            res.sendStatus(404);
+        } else {
+            Nech.find({ by: user._id, type: req.params.type }).then(nechs => {
+                res.json(nechs);
+            });
         }
     });
 });
