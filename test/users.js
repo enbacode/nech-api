6ec9c2b84d5ba47c27c993f50d9104345b57ce69
enba <nick.baas94@gmail.com>
2018-03-21 13:53:25 +0100

    Fix test description

--- test/users.js
+++ test/users.js
@@ -1,21 +1,35 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../app';
-import authHelper from '../authHelper';
 
 chai.use(chaiHttp);
 let should = chai.should(); // eslint-disable-line no-unused-vars
 
 describe('users/me', () => {
-    it('should return a user', done => {
+    it('should return 401 w/o token', done => {
         chai
             .request(server)
             .get('/users/me')
-            .set('Authorization', 'Bearer ' + authHelper.getSuperToken())
             .end((err, res) => {
-                res.should.have.status(200);
-                res.should.be.json;
+                res.should.have.status(401);
                 done();
+            });
+    });
+    it('should return user data after authorizing', done => {
+        chai
+            .request(server)
+            .post('/auth/login')
+            .send({ username: 'user1', password: 'test1234' })
+            .end((loginErr, loginRes) => {
+                chai
+                    .request(server)
+                    .get('/users/me')
+                    .set('Authorization', `Bearer ${loginRes.body.token}`)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.should.be.json;
+                        done();
+                    });
             });
     });
 });
